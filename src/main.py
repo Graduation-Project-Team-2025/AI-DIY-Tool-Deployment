13477d6a6e1e4f90d26c8a7ddfdb980105586f3a
Aya Hazem <ayahazem6103@gmail.com>
2025-06-28 18:32:28 +0300

    cors

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from routes import base_router, diy_router, image_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],               # allow all origins
+    allow_credentials=False,           # optional: allows cookies/auth headers if needed
+    allow_methods=["*"],              # allow all HTTP methods (GET, POST, PUT, etc.)
+    allow_headers=["*"],              # allow all request headers
+)
 app.include_router(base_router)
 app.include_router(diy_router)
 app.include_router(image_router)
